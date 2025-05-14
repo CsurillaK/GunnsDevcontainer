4783c48c42826a7a10f2d9c2d212e8304478041e
Karoly Csurilla <csurilla.karoly@gmail.com>
2025-05-15 01:07:09 +0200

    Minor revision

--- scripts/build_gunns.sh
+++ scripts/build_gunns.sh
@@ -1,9 +1,67 @@
-cd $GUNNS_HOME
-cd ./lib/test
-make "$@"
-cd ../no_trick
-make "$@"
-cd ../trick
-make "$@"
-cd ../trick_if
-make "$@"
+print_usage() {
+    echo "Usage: $0 <no_trick/test/trick/trick_if> <make_settings>"
+    echo "Arguments:"
+    echo "  <no_trick/test/trick/trick_if>  Specify the library to compile:"
+    echo "    - no_trick: /gunns/lib/no_trick"
+    echo "    - test:     /gunns/lib/test"
+    echo "    - trick:    /gunns/lib/trick"
+    echo "    - trick_if: /gunns/lib/trick_if"
+    echo "  <make_settings>                Additional make settings (optional)"
+}
+
+if [ -z "$1" ]; then
+    echo "Error: No library specified."
+    print_usage
+    exit 1
+fi
+
+library_location=""
+case "$1" in
+    no_trick)
+        library_location="./lib/no_trick"
+        ;;
+    test)
+        library_location="./lib/test"
+        ;;
+    trick)
+        library_location="./lib/trick"
+        ;;
+    trick_if)
+        library_location="./lib/trick_if"
+        ;;
+    *)
+        echo "Error: Invalid library specified."
+        print_usage
+        exit 1
+        ;;
+esac
+
+# For detailed description of compilation arguments, please refer to makefiles
+makefile_arguments="${@:2}"
+
+clean_libraries() {
+    cd $GUNNS_HOME
+    cd ./lib/no_trick
+    make clean
+    cd ../test
+    make clean
+    cd ../trick
+    make clean
+    cd ../trick_if
+    make clean
+}
+
+build_library() {
+    clean_libraries
+    
+    cd $GUNNS_HOME
+    cd $library_location
+    if [ -z "$makefile_arguments" ]; then
+        make
+    else
+        make $makefile_arguments
+    fi
+}
+
+# Build the specified library
+build_library
--- scripts/finalize_devcontainer.sh
+++ scripts/finalize_devcontainer.sh
@@ -1,1 +1,8 @@
+# Copy the content of the version controlled .vscode
 cp -r /home/GunnsDevcontainer/.vscode /home/.vscode
+
+# Avoid "dubious ownership" git error
+git config --global --add safe.directory /home/GunnsDevcontainer
+
+# Make scripts executable by VSCode tasks
+chmod +x /home/GunnsDevcontainer/scripts/*.sh
