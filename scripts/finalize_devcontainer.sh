f1faa139d08305754bc8ff90f97e48d239dfdde6
Karoly Csurilla <csurilla.karoly@gmail.com>
2025-07-10 22:39:33 +0000

    Windows line ending fix

--- scripts/finalize_devcontainer.sh
+++ scripts/finalize_devcontainer.sh
@@ -4,5 +4,8 @@ WORKSPACE_DIR="$(cd "${SCRIPT_DIR}/.." && pwd)"
 # Avoid "dubious ownership" git error
 git config --global --add safe.directory ${WORKSPACE_DIR}/gunns
 
+# Correct for Windows line endings
+git config --global core.autocrlf true
+
 # Make scripts executable by VSCode tasks
 chmod +x ${SCRIPT_DIR}/*.sh
